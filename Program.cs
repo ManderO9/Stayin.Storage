8891fcb760eae4a4b83a57f1f0ab9a8e22867808
ManderO9 <hossem.chetoui@gmail.com>
2023-05-18 21:54:50 +0100

    deleted the old way of getting file extensions from mime types

--- Program.cs
+++ Program.cs
@@ -1,81 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using Stayin.Storage;
-using System.Net.Mime;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
+// TODO: delete later
+var corsPolicy = "someCorsPolicy";
+builder.Services.AddCors((options) =>
+{ options.AddPolicy(corsPolicy, policy => { policy.WithOrigins("*").WithHeaders("*").WithMethods("*"); }); });
+
+
 var app = builder.Build();
+app.UseCors(corsPolicy);
 
 // Ensure database exists
 using var scope = app.Services.CreateScope();
 scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.EnsureCreated();
 
 
-app.MapPost("/file/upload",
-    async (IConfiguration configuration,
-            ApplicationDbContext dbContext,
-            HttpResponse response,
-            [FromBody] FileInformation fileInfo) =>
+app.MapPost("/file/upload", async (IConfiguration configuration,
+                                   ApplicationDbContext dbContext,
+                                   HttpResponse response,
+                                   [FromBody] FileInformation fileInfo) =>
+{
+    // The folder to store files in (ends with a trailing slash)
+    var storageFolder = configuration["StorageFolder"];
+
+    // Get file extension
+    var fileExtension = fileInfo.FileType;
+
+    // Create a random name for the file
+    var fileName = Guid.NewGuid().ToString("N");
+
+    // Create the path to store the file in
+    var path = storageFolder + fileName + fileExtension;
+
+    // Store the file on disc
+    await File.WriteAllBytesAsync(path, fileInfo.Content);
+
+    // Create file info to store in the database
+    var newFile = new FileDetails()
     {
-        // The folder to store files in (ends with a trailing slash)
-        var storageFolder = configuration["StorageFolder"];
+        Extension = fileExtension,
+        Id = Guid.NewGuid().ToString("N"),
+        Name = fileName,
+        Size = fileInfo.Content.Count()
+    };
 
-        // Create file type provider
-        var fileExtensionProvider = new FileExtensionContentTypeProvider();
+    // Add the file info to the database
+    dbContext.Files.Add(newFile);
+    await dbContext.SaveChangesAsync();
 
-        // Try get the file extension that maps to the passed in mime type
-        var extensions = fileExtensionProvider.Mappings.Where(x => x.Value.Equals(fileInfo.FileType, StringComparison.OrdinalIgnoreCase));
-
-        // If the passed in mime type is unknown
-        if(extensions.Count() == 0)
-        {
-            // Set error status code
-            response.StatusCode = StatusCodes.Status400BadRequest;
-            
-            // Return an error
-            await response.WriteAsync($"Unknown Mime Type : {fileInfo.FileType}");
-
-            // End response here
-            return;
-        }
-        
-        // Get file extension
-        var fileExtension = extensions.First().Key;
-
-        // Check if it's text and set it directly (cuz multiple mime types have .txt extension)
-        if(extensions.First().Value == MediaTypeNames.Text.Plain)
-            fileExtension = ".txt";
-        
-        // Create a random name for the file
-        var fileName = Guid.NewGuid().ToString("N");
-
-        // Create the path to store the file in
-        var path = storageFolder + fileName + fileExtension;
-
-        // Store the file on disc
-        await File.WriteAllBytesAsync(path, fileInfo.Content);
-
-        // Create file info to store in the database
-        var newFile = new FileDetails()
-        {
-            Extension = fileExtension,
-            Id = Guid.NewGuid().ToString("N"),
-            Name = fileName,
-            Size = fileInfo.Content.Count()
-        };
-
-        // Add the file info to the database
-        dbContext.Files.Add(newFile);
-        await dbContext.SaveChangesAsync();
-
-        // Return file id to the caller
-        await response.WriteAsync(newFile.Id);
-    });
+    // Return file id to the caller
+    await response.WriteAsync(newFile.Id);
+});
 
 
 
@@ -87,7 +68,7 @@ app.MapGet("/file/{fileId}", async (ApplicationDbContext db, IConfiguration conf
     // If the file is null, it doesn't exist
     if(file is null)
         // Return not found
-        return Results.NotFound();
+        return Results.NotFound("The requested file does not exist");
 
     // The folder to store files in (ends with a trailing slash)
     var storageFolder = configuration["StorageFolder"];
